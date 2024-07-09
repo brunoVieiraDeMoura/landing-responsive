e7f511e32d7f6f82b9fc6503e8b594b529ab125f
Bruno Vieira de Moura <bruno.moura.code@gmail.com>
2024-07-09 03:23:43 -0300

    funcionalidade do hamburguer menu + effect aside + effect bg menu

---
+++ js/menu-hamb.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const sidebar = document.getElementById("sidebar");
+  const container = document.querySelector(".container");
+  const bgSideBar = document.getElementById("bg-sidebar");
+
+  hamburger.addEventListener("click", function () {
+    sidebar.classList.toggle("open");
+    if (bgSideBar.classList.contains("fade-in")) {
+      bgSideBar.classList.remove("fade-in");
+      bgSideBar.classList.add("fade-out");
+    } else {
+      bgSideBar.classList.remove("fade-out");
+      bgSideBar.classList.add("fade-in");
+    }
+  });
+
+  container.addEventListener("click", function (e) {
+    if (
+      sidebar.classList.contains("open") &&
+      !sidebar.contains(e.target) &&
+      !hamburger.contains(e.target)
+    ) {
+      sidebar.classList.remove("open");
+      bgSideBar.classList.remove("fade-in");
+      bgSideBar.classList.add("fade-out");
+    }
+  });
+  bgSideBar.classList.add("fade-out");
+});
